a6cdb6e5c53314aa6f437d3a971658c2fd08dac5
hunklesj <hunklesj@mail.uc.edu>
2020-10-16 19:05:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,71 @@
+import com.sun.net.ssl.TrustManagerFactory;
+
+class Main {
+  public static void main(String[] args) {
+    Tree tree1 = new Tree();
+    Tree tree2 = new Tree("maple", "broadleaf", true, "red");
+    tree1.print();
+    tree2.print();
+    tree1.print();
+  }
+}
+  class Tree{
+    private String treeName;
+    private String treeType;
+    private boolean leavesFall;
+    private String leafColor;
+    private String doleavesFall;
+
+    Tree () {
+      treeName ="";
+      treeType = "";
+      leavesFall = false;
+      leafColor = "";
+      if(leavesFall = true){
+        doleavesFall="does";
+      }
+        else{
+          doleavesFall ="does not";
+        }
+      }
+    Tree(String atreeName, String atreeType, boolean aleavesFall, String aleafColor) {
+      treeName =atreeName;
+      treeType = atreeType;
+      leavesFall = aleavesFall;
+      leafColor = aleafColor;
+      if(leavesFall = true){
+        doleavesFall="does";
+      }
+      else{
+        doleavesFall="does not";
+      }
+    }
+    void  setName(String thetreeName){
+      treeName=thetreeName;
+    }
+    void  setType(String thetreeType){
+      treeType = thetreeType;
+    }
+     void setFall(boolean theleavesFall){
+      leavesFall = theleavesFall;
+    }
+    void leafColor(String theleafColor){
+      leafColor=theleafColor;
+    }
+    String getName(){
+      return treeName;
+    }
+    String getType(){
+      return treeType;
+    }
+    boolean getFall(){
+      return leavesFall;
+    }
+    String getleafColor(){
+      return leafColor;
+    }
+    void print(){
+      System.out.println("This is a " + treeName + " Tree. It is a " + treeType + " and its leaves are currenty " + leafColor + ". It " + doleavesFall +" lose its leaves for the winter.");
+    }
+  }
+
